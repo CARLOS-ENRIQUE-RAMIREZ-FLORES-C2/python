100ba87468a5f379d1ab03587f510ecfef237e86
Carlos Ramirez <carlos@soluciones360.com.mx>
2020-04-29 19:28:45 -0500

    se agregaron nuestros primeros programas

---
+++ primer.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.getscreen()
+carlos = turtle.Turtle()
+carlos.forward(50)
+carlos.left(90)
+carlos.forward(50)
+carlos.left(90)
+carlos.forward(50)
+carlos.left(90)
+carlos.forward(50)
+carlos.left(90)
+
+turtle.mainloop()
